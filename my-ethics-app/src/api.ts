5c34fc365d60908740e3baf25afa2afc551b42e4
cajcodes <129537800+cajcodes@users.noreply.github.com>
2024-04-26 18:04:21 -0500

    Update api.ts

--- my-ethics-app/src/api.ts
+++ my-ethics-app/src/api.ts
@@ -1,7 +1,7 @@
 // src/api.ts
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
+const API_URL = 'https://ethical-analysis.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
